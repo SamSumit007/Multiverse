42bd8bca254efec04bf37a4f41eeaf66151b2e1c
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-05-18 17:45:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Card_create from './components/CreateCard';
+import GiftCard from './components/Gift_card';
+
+function App() {
+
+  return (
+    <div className="App">
+     <Card_create />
+     <GiftCard /> 
+        
+        
+
+    </div>
+  );
+}
+
+export default App;
+
